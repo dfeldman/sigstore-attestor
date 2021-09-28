a617017bcc4394b48de5edfe8e6ba1309c02c602
Daniel Feldman <dfeldman.mn@gmail.com>
2021-09-27 23:45:34 -0500

    clean up requirements check

--- scripts/0-check-requirements.sh
+++ scripts/0-check-requirements.sh
@@ -1,2 +1,13 @@
-#!/bin/bash -xeo pipefail
+#!/usr/bin/env bash 
+set -xeo pipefail
+SCRIPTDIR=$(dirname "$0")
+BASEDIR="$SCRIPTDIR/.."
+
 command -v docker || exit "Docker not found"
+command -v go || exit "Go not found"
+
+source ./env
+
+if [ ! -n "${DOCKER_USER}" ]; then
+	echo "Need to set DOCKER_USER to a valid value"
+fi
