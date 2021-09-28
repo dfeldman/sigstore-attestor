123fefe8d835573df9c0d60849fcbb61c261dc62
Daniel Feldman <dfeldman.mn@gmail.com>
2021-09-28 00:01:46 -0500

    Minor fixes

--- scripts/2-build-and-sign-image.sh
+++ scripts/2-build-and-sign-image.sh
@@ -5,7 +5,7 @@ BASEDIR="$SCRIPTDIR/.."
 source $BASEDIR/env
 
 # Remove any old demo image
-docker rmi demo-image
+docker rmi demo-image || echo "Couldn't remove old image"
 
 pushd "$BASEDIR/demo-image"
 
@@ -13,19 +13,18 @@ pushd "$BASEDIR/demo-image"
 echo $(date) >  date
 
 # Build the demo image
-docker build -t ${DOCKER_USER}/demo-image
+docker build -t ${DOCKER_USER}/demo-image .
 
 echo "Sign into Docker Hub (may be cached)"
 docker login
 docker push ${DOCKER_USER}/demo-image:latest
 popd
 
+sleep 2
+
 # Sign the demo image. This is based off a tutorial from here: 
 # https://dev.to/martinheinz/signing-software-the-easy-way-with-sigstore-and-cosign-kde
 COSIGN_EXPERIMENTAL=1 bin/cosign sign \
--oidc-issuer "https://oauth2.sigstore.dev/auth" \
--fulcio-url "https://fulcio.sigstore.dev" \
--rekor-url "https://rekor.sigstore.dev"  \
-docker.io/${DOCKER_USER}/demo-image
+  docker.io/${DOCKER_USER}/demo-image
 
 COSIGN_EXPERIMENTAL=1 bin/cosign verify docker.io/${DOCKER_USER}/demo-image:latest
