178b40d1ceb0b6e8f9a2baf3eb918cc80ac994d9
Daniel Feldman <dfeldman.mn@gmail.com>
2021-09-28 22:08:31 -0500

    script to test attestation

---
+++ scripts/4-attest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+SCRIPTDIR=$(dirname "$0")
+BASEDIR="$SCRIPTDIR/.."
+source $BASEDIR/env
+
+# Run the demo image
+# The command spire-agent api fetch x509 -socketPath /sock/agent.sock is 
+# baked into the image. No conf file is needed for this command.
+docker run --mount "type=bind,src="$(pwd)"/sock,dst=/sock" dfeldman/demo-image:latest
+
